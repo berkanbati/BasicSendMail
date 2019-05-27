a8017b929b3a81171846008e261216fd1dfdbe54
berkanbati <berkanonurbati@gmail.com>
2019-05-27 22:50:23 +0300

    Commit

---
+++ SendMail/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Mail;
+
+namespace SendMail
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public bool Gonder(string konu, string icerik)
+        {
+            MailMessage ePosta = new MailMessage();
+            ePosta.From = new MailAddress("yourgmailadress");
+            ePosta.To.Add(textBox1.Text);
+            ePosta.Subject = konu;
+            ePosta.Body = icerik;
+            SmtpClient smtp = new SmtpClient();
+            smtp.Credentials = new System.Net.NetworkCredential("yourgmailadress", "yourgmailpassword");
+            smtp.Port = 587;
+            smtp.Host = "smtp.gmail.com";
+            smtp.EnableSsl = true;
+            object userState = ePosta;
+            bool kontrol = true;
+            try
+            {
+                smtp.SendAsync(ePosta, (object)ePosta);
+            }
+            catch (SmtpException ex)
+            {
+                kontrol = false;
+                System.Windows.Forms.MessageBox.Show(ex.Message, "Mail Gönderme Hatasi");
+            }
+            return kontrol;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < numericUpDown1.Value; i++)
+            {
+                Gonder(textBox2.Text, richTextBox1.Text);
+            }
+        }
+    }
+}
